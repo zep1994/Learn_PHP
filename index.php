89ad6af38924f5733ff90374bada813f118c1709
Thomas Matlock <snaxbags@protonmail.com>
2020-03-17 16:05:17 -0500

    Setup sessions

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
-<?php
-$cookie_name = "user";
-$cookie_value = "Alex Porter";
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+<?php 
+//Start the session
+session_start();
 ?>
+
+<!DOCTYPE html>
 <html>
-<body>
-
-<?php
-if(!isset($_COOKIE[$cookie_name])) {
-    echo "Cookie named '" . $cookie_name . "' is not set!";
-} else {
-    echo "Cookie '" . $cookie_name . "' is set!<br>";
-    echo "Value is: " . $_COOKIE[$cookie_name];
-}
-?>
-
-</body>
+    <body>
+        <?php 
+            //Set session variables
+        $_SESSION["favcolor"] = "green";
+        $_SESSION["favanimal"] = "cat";
+        echo "Session variables are set.";
+        ?>
+    </body>
 </html>
