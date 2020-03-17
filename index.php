c3cc7fbfcc4da62bdc2d6ebac492c5764c7cb48f
Thomas Matlock <snaxbags@protonmail.com>
2020-03-17 14:55:54 -0500

    Setup upload script

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 <body>
-<?php include "site.php" ?>
-<form action="site.php" method="post" enctype="multipart/form-data">
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
